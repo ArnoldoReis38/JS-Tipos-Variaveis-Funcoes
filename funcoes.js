e3d937bc43f043cb8a18ea6e39c32ee307165830
Arnoldo Reis <arnoldoprofissional@gmail.com>
2022-05-05 20:04:00 -0300

    Aprendendo funções no node.JS

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+const professor = "Rodrigo"
+const professor2 = "Mauro"
+const materia = "fundamentos de estatistica"
+const materia2 = "rede de computadores"
+console.log(`O ${professor} é professor de ${materia}.`)
+
+function substituindo(rede_de_computadores) {
+  console.log(`Porem, o ${professor} está substituindo o professor ${rede_de_computadores} na materia de ${materia2}`)
+}
+substituindo(professor2)
